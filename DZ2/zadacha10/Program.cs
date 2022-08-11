c3ddac0ea1640f9151497e6facc4c27f05321cb8
Elena Utochka <lenochka22@yandex.ru>
2022-08-11 13:32:08 +0300

    exm10

---
+++ DZ2/zadacha10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача10 Поиск 2 цифры в трехзначном числе
+using static System.Console;
+Clear();
+WriteLine("Введите трехзначное число: ");
+int Number = Convert.ToInt32(ReadLine());
+if (Number < 100 || Number > 999)
+{
+    Write("Число не является трехзначным");
+}
+else
+{
+    int temp = Number / 10;
+    Number = temp % 10;
+    Write($"Вторая цифра числа {Number}");
+}
