f872fdb8245ba7324a42b2020e225752797b94d4
Elena Utochka <lenochka22@yandex.ru>
2022-08-17 13:32:32 +0300

    exm29

---
+++ DZ4/zadacha29/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+ и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33] */
+
+using static System.Console;
+//Clear();
+
+int[] Mass = GetArray(8);
+WriteLine($"[{String.Join(", ", Mass)}]");
+
+
+int[] GetArray(int N)
+{
+int[] array = new int[N];
+for (int i=0; i<N; i++)
+{
+    array[i] = new Random().Next(0,10);
+}
+return array;
+} 
