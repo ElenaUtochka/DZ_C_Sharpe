e63723c2a5ff77f03e8f181c773743f2619bf86c
Elena Utochka <lenochka22@yandex.ru>
2022-08-17 12:56:26 +0300

    exm27

---
+++ DZ4/zadacha27/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+
+
+using static System.Console;
+Clear();
+WriteLine("Введите число ");
+int number = int.Parse(ReadLine());
+int[] NewArray = GetArray(number);
+Write($"Сумма цифр в числе {number} равна {SumArray(NewArray)}");
+
+
+
+
+
+int[] GetArray(int N)
+{
+    int Length = 0;
+    int temp = N;
+    while ((temp / 10) > 0)
+    {
+        temp = temp / 10;
+        Length += 1;
+    }
+    int[] Array = new int[Length + 1];
+    for (int i = (Length); i >= 0; i--)
+    {
+        Array[i] = N % 10;
+        N = N / 10;
+    }
+    return Array;
+}
+int SumArray(int[] Mass)
+{
+    int sum = 0;
+    for (int i = 0; i < Mass.Length; i++)
+    {
+        sum += Mass[i];
+    }
+    return sum;
+}
