070f73ecb5cf3e774be4231acbe1481e1cff1288
Elena Utochka <lenochka22@yandex.ru>
2022-08-19 19:35:21 +0300

    exm36

---
+++ DZ5/zadacha36/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+using static System.Console;
+Clear();
+
+WriteLine("Задайте размер массива ");
+int ArrayCount = int.Parse(ReadLine());
+int[] Array= GetArray(ArrayCount);
+WriteLine($"{String.Join(" ", Array)}");
+WriteLine($"Сумма элементов, стоящих на нечетных позициях {GetSum(Array)}");
+
+
+
+
+
+
+
+
+int[] GetArray(int Count)
+{
+    int[] result = new int[Count];
+for (int i = 0; i < Count; i++)
+{
+    result[i]= new Random().Next(-100,100);
+}
+    return result;
+}
+
+int GetSum(int[] massive)
+{
+    int Sum=0;
+for (int i = 1; i < massive.Length; i+=2)
+{
+    Sum+=massive[i];
+}
+    return Sum;
+}
