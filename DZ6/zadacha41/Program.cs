9d2c14a68615be2dbec0e62014745e68a74ac295
Elena Utochka <lenochka22@yandex.ru>
2022-08-23 14:53:46 +0300

    exm41

---
+++ DZ6/zadacha41/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите числа через пробел: ");
+int[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+WriteLine(String.Join(" ", array));
+WriteLine($"Чисел больше нуля: {CountPositive(array)}");
+
+
+
+
+
+
+
+
+int CountPositive(int[] mass)
+    {
+        int count = 0;
+        for (int i = 0; i < mass.Length; i++)
+        {
+            count = mass[i] > 0 ? count + 1 : count + 0;
+        }
+        return count;
+    }
