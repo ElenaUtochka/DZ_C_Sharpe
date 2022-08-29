c166e785a9fe3c68018561729aef5f12286ea817
Elena Utochka <lenochka22@yandex.ru>
2022-08-29 10:13:03 +0300

    exm47.1

--- DZ7/zadacha47/Program.cs
+++ DZ7/zadacha47/Program.cs
@@ -9,11 +9,11 @@ using static System.Console;
 using System.Linq;
 Clear();
 WriteLine("Введите через пробел размеры массива: ");
-int[] parametrs=ReadLine()!.
+int[] parametrs = ReadLine()!.
                 Split(" ", StringSplitOptions.RemoveEmptyEntries).
-                Select(x=>int.Parse(x)).
+                Select(x => int.Parse(x)).
                 ToArray();
-double[,] array=GetArray(parametrs[0], parametrs[1]);
+double[,] array = GetArray(parametrs[0], parametrs[1]);
 PrintArray(array);
 
 
@@ -29,15 +29,15 @@ PrintArray(array);
 
 double[,] GetArray(int rows, int columns)
 {
-double[,] result = new double[rows,columns];
-for (int i = 0; i < rows; i++)
-{
-    for (int j = 0; j < columns; j++)
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
     {
-        result[i,j]=new Random().Next(-9,10)+Math.Round(new Random().NextDouble(),1);
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(-9, 10) + Math.Round(new Random().NextDouble(), 1);
+        }
     }
-}
-return result;
+    return result;
 }
 
 void PrintArray(double[,] matrix)
@@ -46,7 +46,7 @@ void PrintArray(double[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Write($"{matrix[i,j]} ");
+            Write($"{matrix[i, j]} ");
         }
         WriteLine();
     }
